5b8affd054db9c9252abe4d7a50976de44f874ae
nasa <htilcs1115@gmail.com>
2019-01-13 19:52:43 +0900

    Add fetch_branches function

--- src/switch.rs
+++ src/switch.rs
@@ -8,6 +8,32 @@ pub fn exec(matches: &ArgMatches) {
 
     // output to stdout select branch
     let selection_branch = get_selection_branch(branches);
+type Branches = Vec<String>;
+type BranchName = String;
+
+fn fetch_branches() -> Branches {
+    let repo = Repository::open(".").expect("failed: open repository");
+
+    let mut branches = Vec::new();
+
+    let filter = None;
+    for branch in repo.branches(filter).expect("failed: fetch branches") {
+        let (branch, _) = branch.unwrap();
+
+        // Result<Option<String>という構造になっている
+        // unwrap().unwrap() するのも分かりづらいのでifで剥がす
+        let name = if let Ok(Some(name)) = branch.name() {
+            name
+        } else {
+            panic!("failed")
+        };
+
+        branches.push(name.to_string())
+
+    }
+
+    branches
+}
 
     // git checkout select_branch
     checkout(selection_branch);
