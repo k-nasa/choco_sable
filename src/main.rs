2b7acf5d5f77fa45741d0444b5db37cce9099865
nasa <htilcs1115@gmail.com>
2019-01-13 18:36:22 +0900

    Add main logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+mod remove;
+mod switch;
+
+use clap::*;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = app().get_matches();
+
+    match matches.subcommand() {
+        ("switch", Some(matches)) => switch::exec(matches),
+        ("remove", Some(matches)) => remove::exec(matches),
+        _ => app().print_help().unwrap(),
+    }
+}
+
+fn app() -> App<'static, 'static> {
+    App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .subcommand(SubCommand::with_name("switch").arg(Arg::with_name("branch").required(true)))
+        .subcommand(SubCommand::with_name("remove").arg(Arg::with_name("branches").multiple(true)))
 }
