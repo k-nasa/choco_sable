c202bfcbd4ecef8bc0558773756fd4423df3aecf
nasa <htilcs1115@gmail.com>
2019-01-13 18:46:04 +0900

    Add git switch main logic

--- src/switch.rs
+++ src/switch.rs
@@ -1,3 +1,14 @@
 use clap::*;
+use dialoguer::*;
+use git2::*;
 
-pub fn exec(matches: &ArgMatches) {}
+pub fn exec(matches: &ArgMatches) {
+    // fetch local repository branches
+    let branches = fetch_branches();
+
+    // output to stdout select branch
+    let selection_branch = get_selection_branch(branches);
+
+    // git checkout select_branch
+    checkout(selection_branch);
+}
