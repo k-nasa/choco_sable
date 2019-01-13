e1c24b871933b3832dfd484414e521f991c2bc34
nasa <htilcs1115@gmail.com>
2019-01-13 20:46:24 +0900

    Add remove subcommand

--- src/remove.rs
+++ src/remove.rs
@@ -1,3 +1,20 @@
-use clap::*;
+use crate::util::*;
+use std::process::Command;
 
-pub fn exec(matches: &ArgMatches) {}
+pub fn exec() {
+    let branches = fetch_branches();
+    let selection_branch = get_selection_branch(&branches);
+
+    delete_branch(&selection_branch);
+}
+
+fn delete_branch(branch: &BranchName) {
+    let mut editor_process = Command::new("git")
+        .arg("branch")
+        .arg("-D")
+        .arg(branch)
+        .spawn()
+        .unwrap();
+
+    editor_process.wait().unwrap();
+}
