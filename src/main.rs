5e9023222dcbc4bf2730a9200ac9f21a6cbfacc1
nasa <htilcs1115@gmail.com>
2019-01-13 21:00:48 +0900

    fix: change crate name

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn main() {
 fn app() -> App<'static, 'static> {
     App::new(crate_name!())
         .version(crate_version!())
-        .author(crate_authors!())
+        .about(crate_description!())
         .subcommand(SubCommand::with_name("switch"))
         .subcommand(SubCommand::with_name("remove"))
 }
